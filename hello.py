741b8c7c7dc352a121befe5ee27d7803ddeab254
Gabriel Sierra <193622299+g-sierra@users.noreply.github.com>
2025-06-29 21:49:33 -0300

    Created goodbye.py script importing a function from hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello world!")
+def getName():
+    name = str(input("Enter your name: "))
+    return name
+
+def main():
+    print(f"Hello {getName()}!")
+
+if __name__ == "__main__":
+    main()
